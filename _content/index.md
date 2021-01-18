ce6026c781c6a7e648b601e71668857d873bff9e
sielay <lukaszsielski@gmail.com>
2021-01-18 13:57:37 +0000

    Initial

---
+++ _content/index.md
@@ -0,0 +1,24 @@
+---
+title: Becoming a Software Developer - A prequel to learn-to-code books in a rapidly changing world
+topNav: false
+---
+{%- include 'hero.njk' -%}
+{%- include 'subscribe.njk' -%}
+<div>
+  <ul>
+    {%- for post in collections.blog[0].items  -%}
+    <li>
+      {%- include 'tile.njk' -%}
+    </li>
+    {%- endfor -%}
+  </ul>
+  <a href="/blog/" class="bg-yellow-400 block shadow-2xl p-2 px-4">More blog post</a>
+</div>
+<div class="flex flex-wrap">
+<div class="lg:w-1/2 xl:w-1/2 p-6">
+{%- include 'months.njk' -%}
+</div>
+<div class="lg:w-1/2 xl:w-1/2 p-6">
+{%- include 'tagcloud.njk' -%}
+</div>
+</div>
---
+++ functions/submission-created.js
@@ -0,0 +1,107 @@
+const { parse } = require("querystring");
+const axios = require("axios");
+const mailChimpAPI = process.env.MAILCHIMP_API_KEY;
+const mailChimpListID = process.env.MAILCHIMP_LIST_ID;
+
+exports.handler = (event, context, callback) => {
+  try {
+    const {
+      payload: { email },
+    } = JSON.parse(event.body);
+
+    if (!email) {
+      console.log("missing email");
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "missing email",
+        }),
+      });
+    }
+
+    if (!mailChimpAPI) {
+      console.log("missing mailChimpAPI key");
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "missing mailChimpAPI key",
+        }),
+      });
+    }
+
+    if (!mailChimpListID) {
+      console.log("missing mailChimpListID key");
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "missing mailChimpListID key",
+        }),
+      });
+    }
+
+    const data = {
+      email_address: email,
+      status: "pending",
+      merge_fields: {},
+    };
+
+    const subscriber = JSON.stringify(data);
+    console.log("Sending data to mailchimp", subscriber);
+
+    // Subscribe an email
+
+    axios({
+      method: "post",
+      url: `https://us7.api.mailchimp.com/3.0/lists/${mailChimpListID}/members/`, //change region (us19) based on last values of ListId.
+      data: subscriber,
+      auth: {
+        username: "apikey", // any value will work
+        password: mailChimpAPI,
+      },
+    })
+      .then(function (response) {
+        console.log(`status:${response.status}`);
+        console.log(`data:${response.data}`);
+        console.log(`headers:${response.headers}`);
+
+        if (
+          response.headers["content-type"] ===
+          "application/x-www-form-urlencoded"
+        ) {
+          // Do redirect for non JS enabled browsers
+          return callback(null, {
+            statusCode: 302,
+            headers: {
+              Location: "/thanks.html",
+              "Cache-Control": "no-cache",
+            },
+            body: JSON.stringify({}),
+          });
+        }
+
+        // Return data to AJAX request
+        return callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ emailAdded: true }),
+        });
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
