c4bde02d090e1a5700359740e512cfb07474050c
Łukasz Marek Sielski <lukaszsielski@gmail.com>
2022-12-22 11:54:05 +0000

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -5,7 +5,8 @@ const mailChimpListID = process.env.MAILCHIMP_LIST_ID;
 
 exports.handler = (event, context, callback) => {
   try {
-    console.log('What comes', JSON.stringify(event, null, 2));
+    console.log('Hi!');
+    console.log('What comes', JSON.stringify(event.body, null, 2));
     const {
       payload: { email, message },
     } = JSON.parse(event.body);
